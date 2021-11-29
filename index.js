e2c88c4b39ead84ccbf64330628b6c7bbe593a5a
Parsa <parsa.firoozi55@gmail.com>
2021-11-29 16:24:23 +0330

    Update index.js

--- index.js
+++ index.js
@@ -11,58 +11,71 @@ const app = new Discord.Client()
 app.commands = new Discord.Collection();
 app.aliases = new Discord.Collection();
 
-app.on('ready', () => {
+app.on('ready', () => 
+{
     console.log(`| ${app.user.tag} is online !!!`)
 
-setInterval(function() {
-    app.user.setActivity('Discord JS app 1', { type: "WATCHING" })
-    app.user.setActivity('Discord JS app 2', { type: "WATCHING" })
-    app.user.setActivity('Discord JS app 3', { type: "WATCHING" })
-},4000)
-    
+    setInterval(function()
+    {
+        app.user.setActivity('Discord JS app 1', { type: "WATCHING" })
+        app.user.setActivity('Discord JS app 2', { type: "WATCHING" })
+        app.user.setActivity('Discord JS app 3', { type: "WATCHING" })
+    }, 4000)
 })
 
-fs.readdir("./commands/", (err, files) => {
-
+fs.readdir("./commands/", (err, files) => 
+{
     if(err) console.log(err);
     let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if(jsfile.length <= 0){
+    if(jsfile.length <= 0)
+    {
       console.log("Couldn't find commands.");
       return;
     }
     
-    jsfile.forEach((f, i) =>{
+    jsfile.forEach((f, i) =>
+    {
       let props = require(`./commands/${f}`);
+        
       console.log(`| --------Commands-------- `);
       console.log(`| ✅ ${f} loaded! `);
+        
       app.commands.set(props.help.name, props);
-      props.help.aliases.forEach(alias => { 
+        
+      props.help.aliases.forEach(alias =>
+      { 
         app.aliases.set(alias, props.help.name);
       });
     });
   })
 
-app.on("message", async message => {
-
+app.on("message", async message => 
+{
     let messageArray = message.content.split(" ");
     let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
     let commandfile;
   
-    if (app.commands.has(cmd)) {
-      commandfile = app.commands.get(cmd);
-  } else if (app.aliases.has(cmd)) {
-    commandfile = app.commands.get(app.aliases.get(cmd));
-  }
+    if (app.commands.has(cmd))
+    {
+        commandfile = app.commands.get(cmd);
+    }
+    else if (app.aliases.has(cmd)) 
+    {
+        commandfile = app.commands.get(app.aliases.get(cmd));
+    }
   
-      if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) return;
   
           
-  try {
+  try 
+  {
     commandfile.run(app, message, args);
-  
-  } catch (e) {
-  }}
-  )
+  } 
+  catch (e) 
+  {
+    console.log(e)
+  }
+})
 
 app.login(token)
